ec1babbec2dde8a7ddb923d25f846ffd4a187c84
shivansh-653 <shivansh2003patel@gmail.com>
2025-03-04 21:37:59 +0530

    fixed bug in JS

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
-let progress=document.getElementById('progress');
-let song=document.getElementById('song');
-let ctrlIcon=document.getElementById('ctrlIcon');
+let progress = document.getElementById('progress');
+let song = document.getElementById('song');
+let ctrlIcon = document.getElementById('ctrlIcon');
 
-song.onloadedmetadata=function(){
-    progress.max=song.duration;
-    progress.value=song.currentTime;
+song.onloadedmetadata = function () {
+    progress.max = song.duration;
+    progress.value = 0;
 }
 
-function playPause(){
-    if(ctrlIcon.classList.contains('fa-pause')){
+function playPause() {
+    if (ctrlIcon.classList.contains('fa-pause')) {
         song.pause();
         ctrlIcon.classList.remove("fa-pause");
         ctrlIcon.classList.add("fa-play");
     }
-    else{
+    else {
         song.play();
         ctrlIcon.classList.remove("fa-play");
         ctrlIcon.classList.add("fa-pause");
     }
 }
 
-if(song.play()){
-    setInterval(() => {
-        progress.value=song.currentTime;
-    }, 500);
-}
+// if(song.play()){
+setInterval(() => {
+    progress.value = song.currentTime;
+}, 500);
+// }
 
-progress.onchange = function(){
-    song.play();
-    song.currentTime=progress.value;
-    ctrlIcon.classList.remove("fa-play");
-    ctrlIcon.classList.add("fa-pause");
+progress.onchange = function () {
+    song.currentTime = progress.value;
+    if (ctrlIcon.classList.contains('fa-play')) {
+        song.play();
+        ctrlIcon.classList.remove("fa-play");
+        ctrlIcon.classList.add("fa-pause");
+    }
 }
